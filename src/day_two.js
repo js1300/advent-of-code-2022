991cf3dfbc5eaf7612a499450d336e0214f97426
Jonathan Simmonds <js1300@hotmail.co.uk>
2022-12-05 21:31:00 +0000

    Completed day two part two

--- src/day_two.js
+++ src/day_two.js
@@ -7,10 +7,54 @@ class DayTwo {
     total_score += games.map(this.getSelectionScore).reduce((a, b) => a + b, 0);
     total_score += games.map(this.getResultScore).reduce((a, b) => a + b, 0);
 
-    return { part_one: total_score, part_two: 0 };
+    var converted_games = games.map(this.convertOutcomeToSelection);
+    var converted_total_score = 0;
+
+    converted_total_score += converted_games.map(this.getSelectionScore).reduce((a, b) => a + b, 0);
+    converted_total_score += converted_games.map(this.getResultScore).reduce((a, b) => a + b, 0);
+
+    return { part_one: total_score, part_two: converted_total_score };
   }
 
   // TODO: refactor these
+
+  static convertOutcomeToSelection(game) {
+    switch(game[0]) {
+      case "A":
+        switch(game[2]) {
+          case "X":
+            return "A Z";
+          case "Y":
+            return "A X";
+          case "Z":
+            return "A Y";
+        }
+
+        break;
+      case "B":
+        switch(game[2]) {
+          case "X":
+            return "B X";
+          case "Y":
+            return "B Y";
+          case "Z":
+            return "B Z";
+        }
+
+        break;
+      case "C":
+        switch(game[2]) {
+          case "X":
+            return "C Y";
+          case "Y":
+            return "C Z";
+          case "Z":
+            return "C X";
+        }
+
+        break;
+    }
+  }
 
   static getSelectionScore(game) {
     switch(game[2]) {
