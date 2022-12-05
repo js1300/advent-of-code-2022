a7fe7d318aa48c9ab84e8c9c9f7aeede58e2c31b
Jonathan Simmonds <js1300@hotmail.co.uk>
2022-12-05 22:47:01 +0000

    Refactored another method

--- src/day_two.js
+++ src/day_two.js
@@ -16,43 +16,12 @@ class DayTwo {
     return { part_one: total_score, part_two: converted_total_score };
   }
 
-  // TODO: refactor this
   static convertOutcomeToSelection(game) {
-    switch(game[0]) {
-      case "A":
-        switch(game[2]) {
-          case "X":
-            return "A Z";
-          case "Y":
-            return "A X";
-          case "Z":
-            return "A Y";
-        }
+    var cpu = {A: 2, B: 0, C: 1};
+    var player = {X: 0, Y: 1, Z: 2};
+    var selections = ["X", "Y", "Z"];
 
-        break;
-      case "B":
-        switch(game[2]) {
-          case "X":
-            return "B X";
-          case "Y":
-            return "B Y";
-          case "Z":
-            return "B Z";
-        }
-
-        break;
-      case "C":
-        switch(game[2]) {
-          case "X":
-            return "C Y";
-          case "Y":
-            return "C Z";
-          case "Z":
-            return "C X";
-        }
-
-        break;
-    }
+    return game[0] + " " + selections[(cpu[game[0]] + player[game[2]]) % 3];
   }
 
   // TODO: refactor this
