11aa09062a6db522c728f20c06e0f2bae6bed6b0
Jonathan Simmonds <js1300@hotmail.co.uk>
2022-12-07 21:04:45 +0000

    Completed day three part two

--- src/day_three.js
+++ src/day_three.js
@@ -18,13 +18,38 @@ class DayThree {
         return true;
       });
     });
+
+    var groups = this.groupRucksacks(rucksacks, 3);
+    var group_priority_sum = 0;
+
+    groups.forEach((group) => {
+      [...group[0]].every((item) => {
+        if (group[1].includes(item) && group[2].includes(item)) {
+          group_priority_sum += this.retrieveItemPriority(item);
+
+          return false;
+        }
+
+        return true;
+      });
+    });
     
-    return { part_one: priority_sum, part_two: 0 };
+    return { part_one: priority_sum, part_two: group_priority_sum };
   }
 
   static retrieveItemPriority(item) {
     var char_code = item.charCodeAt(0);
 
     return item.toUpperCase() === item ? char_code - 38 : char_code - 96;
+  }
+
+  static groupRucksacks(rucksacks, group_size) {
+    var groups = new Array();
+
+    for (var i = 0; i < rucksacks.length; i += group_size) {
+      groups.push(rucksacks.slice(i, i + group_size));
+    }
+
+    return groups;
   }
 }
