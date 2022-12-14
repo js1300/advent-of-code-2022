55ce006562043f838983124ee516666d4edf5672
Jonathan Simmonds <js1300@hotmail.co.uk>
2022-12-14 13:37:35 +0000

    Day 12 part one and two solutions

--- src/day_twelve.js
+++ src/day_twelve.js
@@ -7,9 +7,6 @@ class DayTwelve {
     var elevations = new Array();
     var start = {x: 0, y: 0};
     var end = {x: 0, y: 0};
-    var active_paths = new Array();
-    var wrong_paths = new Array();
-    var completed_paths = new Array();
 
     for (var i = 0; i < rows.length; i++) {
       var start_column = rows[i].indexOf("S");
@@ -30,140 +27,135 @@ class DayTwelve {
       elevations.push(rows[i].split('').map(e => e.charCodeAt(0) - 96));
     }
 
-    active_paths.push({id: this.uuidv4(), x: start.x, y: start.y, steps: new Array()});
-    var path_ids_to_remove = new Array();
-    var new_active_paths = new Array();
+    var visited = Array.from({length: elevations.length}, () => Array.from({length: elevations[0].length}, () => false));
+    var part_one_result = this.breadth_first_search(start, end, elevations, visited, false);
 
-    while (active_paths.length > 0) {
-      active_paths.forEach((path) => {
-        var possible_moves = ["L", "R", "U", "D"];
-        var impossible_moves = new Array();
+    visited = Array.from({length: elevations.length}, () => Array.from({length: elevations[0].length}, () => false));
+    var part_two_result = this.breadth_first_search(end, {elevation: 1}, elevations, visited, true);
+    
+    return { part_one: part_one_result, part_two: part_two_result };
+  }
 
-        if (path.x == 0) {
-          impossible_moves.push("L");
-        } else if (path.x == rows[0].length - 1) {
-          impossible_moves.push("R");
-        }
-        if (path.y == 0) {
-          impossible_moves.push("U");
-        } else if (path.y == rows.length - 1) {
-          impossible_moves.push("D");
-        }
+  static breadth_first_search(start, end, elevations, visited, reversed) {
+    var queue = new Array();
 
-        possible_moves = possible_moves.filter(move => !impossible_moves.includes(move));
-        var args = {path: path, elevations: elevations};
-        possible_moves = possible_moves.filter(this.check_elevation_scalability, args);
+    queue.push({x: start.x, y: start.y, steps: 0});
+    visited[start.y][start.x] = true;
 
-        if (possible_moves.length > 0) {
-          if (possible_moves.length > 1) {
-            for (var i = 1; i < possible_moves.length; i++) {
-              var new_path = JSON.parse(JSON.stringify(path));
-              new_path.id = this.uuidv4();
-
-              this.make_move(new_path, possible_moves[i]);
-              var coordinate = new_path.x + "," + new_path.y;
-              
-              if (new_path.steps.includes(coordinate)) {
-                wrong_paths.push(new_path);
-              } else {
-                new_path.steps.push(coordinate);
-
-                if (new_path.x == end.x && new_path.y == end.y) {
-                  completed_paths.push(new_path);
-                } else {
-                  new_active_paths.push(new_path);
-                }
-              }
-            }
-          }
-
-          this.make_move(path, possible_moves[0]);
-          var coordinate = path.x + "," + path.y;
-              
-          if (path.steps.includes(coordinate)) {
-            wrong_paths.push(path);
-            path_ids_to_remove.push(path.id);
-          } else {
-            path.steps.push(coordinate);
-
-            if (path.x == end.x && path.y == end.y) {
-              completed_paths.push(path);
-              path_ids_to_remove.push(path.id);
-            }
-          }
-        } else {
-          wrong_paths.push(path);
-          path_ids_to_remove.push(path.id);
-        }
-      });
-
-      active_paths = active_paths.filter(path => !path_ids_to_remove.includes(path.id));
-      path_ids_to_remove = new Array();
+    while (queue.length > 0) {
+      var current = queue[0];
       
-      active_paths = active_paths.concat(new_active_paths);
-      new_active_paths = new Array();
+      if (current.x == end.x && current.y == end.y) {
+        return current.steps;
+      }
 
-      console.log("********************");
-      console.log("Active paths: " + active_paths.length);
-      console.log("Wrong paths: " + wrong_paths.length);
-      console.log("Completed paths: " + completed_paths.length);
+      if (Object.hasOwn(end, "elevation")) {
+        if (elevations[current.y][current.x] == end.elevation) {
+          return current.steps;
+        }
+      } else {
+        if (current.x == end.x && current.y == end.y) {
+          return current.steps;
+        }
+      }
+
+      var possible_moves = this.calculate_possible_moves(current, elevations, visited, reversed);
+
+      if (possible_moves.includes("L")) {
+        queue.push({x: current.x - 1, y: current.y, steps: current.steps + 1});
+        visited[current.y][current.x - 1] = true;
+      }
+      if (possible_moves.includes("R")) {
+        queue.push({x: current.x + 1, y: current.y, steps: current.steps + 1});
+        visited[current.y][current.x + 1] = true;
+      }
+      if (possible_moves.includes("U")) {
+        queue.push({x: current.x, y: current.y - 1, steps: current.steps + 1});
+        visited[current.y - 1][current.x] = true;
+      }
+      if (possible_moves.includes("D")) {
+        queue.push({x: current.x, y: current.y + 1, steps: current.steps + 1});
+        visited[current.y + 1][current.x] = true;
+      }
+
+      queue.shift();
+    }
+  }
+
+  static calculate_possible_moves(coordinate, elevations, visited, reversed) {
+    var possible_moves = ["L", "R", "U", "D"];
+    var impossible_moves = new Array();
+
+    if (coordinate.x == 0) {
+      impossible_moves.push("L");
+    } else if (coordinate.x == elevations[0].length - 1) {
+      impossible_moves.push("R");
+    }
+    if (coordinate.y == 0) {
+      impossible_moves.push("U");
+    } else if (coordinate.y == elevations.length - 1) {
+      impossible_moves.push("D");
     }
 
-    completed_paths.sort((a, b) => a.steps - b.steps);
-
-    return { part_one: completed_paths.length > 0 ? completed_paths[0].steps.length : 0, part_two: 0 };
+    possible_moves = possible_moves.filter(move => !impossible_moves.includes(move));
+    var args = {coordinate: coordinate, visited: visited};
+    possible_moves = possible_moves.filter(this.check_unvisited, args);
+    args = {coordinate: coordinate, elevations: elevations, reversed: reversed};
+    return possible_moves.filter(this.check_elevation_scalability, args);
   }
 
   static check_elevation_scalability(move) {
-    var current_elevation = this["elevations"][this["path"].y][this["path"].x];
+    var current_elevation = this["elevations"][this["coordinate"].y][this["coordinate"].x];
     var target_elevation = null;
 
     switch (move) {
       case "L":
-        target_elevation = this["elevations"][this["path"].y][this["path"].x - 1];
+        target_elevation = this["elevations"][this["coordinate"].y][this["coordinate"].x - 1];
 
         break;
       case "R":
-        target_elevation = this["elevations"][this["path"].y][this["path"].x + 1];
+        target_elevation = this["elevations"][this["coordinate"].y][this["coordinate"].x + 1];
         
         break;
       case "U":
-        target_elevation = this["elevations"][this["path"].y - 1][this["path"].x];
+        target_elevation = this["elevations"][this["coordinate"].y - 1][this["coordinate"].x];
 
         break;
       case "D":
-        target_elevation = this["elevations"][this["path"].y + 1][this["path"].x];
+        target_elevation = this["elevations"][this["coordinate"].y + 1][this["coordinate"].x];
 
         break;
     }
 
-    return (current_elevation + 1) >= target_elevation;
+    if (this["reversed"]) {
+      return current_elevation <= target_elevation + 1;
+    } else {
+      return (current_elevation + 1) >= target_elevation;
+    }
   }
 
-  static make_move(path, move) {
+  static check_unvisited(move) {
+    var result = true;
+
     switch (move) {
       case "L":
-        path.x -= 1;
+        result = this["visited"][this["coordinate"].y][this["coordinate"].x - 1];
 
         break;
       case "R":
-        path.x += 1;
-
+        result = this["visited"][this["coordinate"].y][this["coordinate"].x + 1];
+        
         break;
       case "U":
-        path.y -= 1;
+        result = this["visited"][this["coordinate"].y - 1][this["coordinate"].x];
 
         break;
       case "D":
-        path.y += 1;
+        result = this["visited"][this["coordinate"].y + 1][this["coordinate"].x];
 
         break;
     }
-  }
 
-  static uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
+    return !result;
   }
 }
