e136a6d079de1421a7ca596efe51c21b259386fe
Jonathan Simmonds <js1300@hotmail.co.uk>
2022-12-15 14:04:22 +0000

    Day six part one

---
+++ src/day_six.js
@@ -0,0 +1,32 @@
+class DaySix {
+
+  static execute() {
+    var data = day_six_data;
+    var scan_length = 4;
+    var first_marker = 0;
+
+    for (var i = 0; i < data.length - scan_length; i++) {
+      var scan_content = data.substring(i, i + scan_length);
+      var match_found = false;
+
+      for (var j = 0; j < scan_length - 1; j++) {
+        var left_hand_side = scan_content.charAt(j);
+        var right_hand_side = scan_content.substring(j + 1);
+
+        match_found = right_hand_side.includes(left_hand_side);
+
+        if (match_found) {
+          break;
+        }
+      }
+
+      if (!match_found) {
+        first_marker = i + scan_length;
+        
+        break;
+      }
+    }
+
+    return { part_one: first_marker, part_two: 0 };
+  }
+}
